f62ad3864ae129e69600c95fe32aef218bebc35f
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-17 15:28:31 +0900

    feat: db connection

--- src/api/schema.py
+++ src/api/schema.py
@@ -1,8 +1,20 @@
 import strawberry
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from strawberry.fastapi import GraphQLRouter
 
 from src.api.mutation import Mutation
 from src.api.query import Query
+from src.database.db import db
+
+
+def get_context(
+    session: AsyncSession = Depends(db.get_db),
+) -> dict[str, AsyncSession]:
+    return {"session": session}
+
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
-graphql_app = GraphQLRouter[object, object](schema=schema)
+graphql_app = GraphQLRouter[object, object](
+    schema=schema, context_getter=get_context
+)
---
+++ src/database/db.py
@@ -0,0 +1,40 @@
+from typing import AsyncIterator, Optional
+
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from src.database.config import db_config
+
+
+class Database:
+    def __init__(self) -> None:
+        self.__session: Optional[async_sessionmaker[AsyncSession]] = None
+        self.__engine: Optional[AsyncEngine] = None
+
+    def connect(self) -> None:
+        self.__engine = create_async_engine(url=db_config.dsn)
+
+        self.__session = async_sessionmaker(
+            bind=self.__engine,
+            autocommit=False,
+        )
+
+    async def disconnect(self) -> None:
+        if self.__engine is None:
+            raise AttributeError
+        await self.__engine.dispose()
+
+    async def get_db(
+        self,
+    ) -> AsyncIterator[AsyncSession]:
+        if self.__session is None:
+            raise AttributeError
+        async with self.__session() as session:
+            yield session
+
+
+db = Database()
