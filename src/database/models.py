07ca45ae6428298cfcbb87efbfa7c14ff82d0298
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-17 12:27:24 +0900

    feat: sqlalchemy model

---
+++ src/database/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Country(Base):
+    __tablename__ = "countries"
+
+    country_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    country_name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    cities: Mapped[list["City"]] = relationship(backref="cities")
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    city_id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    country_id: Mapped[int] = mapped_column(ForeignKey("countries.country_id"))
+    city_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    population: Mapped[int] = mapped_column(Integer, nullable=True)
