913a549f0a9c8a09bb6fa99827b7f3d37efabfe7
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-20 08:56:23 +0900

    test: stmts

--- tests/api/resolvers/test_stmts.py
+++ tests/api/resolvers/test_stmts.py
@@ -1,7 +1,8 @@
 import pytest
 
-from src.api.resolvers.stmts import get_countries
+from src.api.resolvers.stmts import get_cities, get_countries
 from src.database.db import db
+from tests.seed import cities as expected_cities
 from tests.seed import countries as expected_coutries
 
 
@@ -13,3 +14,13 @@ async def test_get_countries(init_db: None) -> None:
     async with db.session() as session:
         actual = await get_countries(session=session)
         assert actual == expected_coutries
+
+
+@pytest.mark.asyncio
+async def test_get_cities(init_db: None) -> None:
+    db.connect()
+    if db.session is None:
+        raise ValueError("session not found.")
+    async with db.session() as session:
+        actual = await get_cities(session=session)
+        assert actual == expected_cities
