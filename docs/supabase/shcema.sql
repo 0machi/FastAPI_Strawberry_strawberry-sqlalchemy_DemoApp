65f3aea0abdd78d964c98a4cf4c34cabc0cf2750
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-22 10:42:07 +0900

    delete: docs

--- docs/supabase/shcema.sql
+++
@@ -1,9 +0,0 @@
--- 下記を実行後、settings/api/Exposed schemasにdevを追加
-CREATE SCHEMA dev;
-GRANT USAGE ON SCHEMA dev TO anon, authenticated, service_role;
-GRANT ALL ON ALL TABLES IN SCHEMA dev TO anon, authenticated, service_role;
-GRANT ALL ON ALL ROUTINES IN SCHEMA dev TO anon, authenticated, service_role;
-GRANT ALL ON ALL SEQUENCES IN SCHEMA dev TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON TABLES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON ROUTINES TO anon, authenticated, service_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA dev GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
