c5a3497d6be975829b457f20e05b73ebc36fba97
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-23 10:47:50 +0900

    test: resolver stmt dir

--- tests/api/resolvers/stmts/test_country_stmt.py
+++ tests/api/resolvers/stmts/test_country_stmt.py
@@ -1,6 +1,5 @@
 import pytest
 
-from src.api.resolvers.stmts.city_stmt import get_cities
 from src.api.resolvers.stmts.country_stmt import (
     add_country,
     delete_country,
@@ -10,7 +9,6 @@ from src.api.resolvers.stmts.country_stmt import (
 )
 from src.database.models import Country
 from src.database.session_manager import DatabaseSessionManager
-from tests.seed import cities as expected_cities
 from tests.seed import countries as expected_coutries
 from tests.seed import us
 
@@ -20,13 +18,6 @@ async def test_get_countries(session: DatabaseSessionManager) -> None:
     async with session.session() as s:
         actual = await get_countries(session=s)
         assert actual == expected_coutries
-
-
-@pytest.mark.asyncio
-async def test_get_cities(session: DatabaseSessionManager) -> None:
-    async with session.session() as s:
-        actual = await get_cities(session=s)
-        assert actual == expected_cities
 
 
 @pytest.mark.asyncio
