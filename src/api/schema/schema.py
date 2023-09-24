3cdc0e9c341baaacdcd5c3560fa67bbe73c8b230
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-24 16:42:58 +0900

    refactor: get_context

--- src/api/schema/schema.py
+++ src/api/schema/schema.py
@@ -1,23 +1,13 @@
 import strawberry
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 from strawberry.fastapi import GraphQLRouter
 from strawberry.tools import merge_types
 
-from src.api.resolvers.context import ContextType
+from src.api.resolvers.context import get_context
 from src.api.schema.mutations.country_mutation import CountryMutation
 from src.api.schema.mutations.user_mutation import UserMutation
 from src.api.schema.queries.city_query import CityQuery
 from src.api.schema.queries.country_query import CountryQuery
 from src.api.schema.types import strawberry_sqlalchemy_mapper
-from src.database.session_manager import get_db
-
-
-def get_context(
-    session: AsyncSession = Depends(get_db),
-) -> ContextType:
-    return {"session": session}
-
 
 strawberry_sqlalchemy_mapper.finalize()
 additional_types = list(strawberry_sqlalchemy_mapper.mapped_types.values())
