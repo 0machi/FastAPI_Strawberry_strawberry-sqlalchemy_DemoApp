063e09eddccd4b3d2859a3d246592a865f4c4637
Kohei Omachi <koomachi@yahoo-corp.jp>
2023-09-24 16:55:19 +0900

    test: country_query

--- src/api/resolvers/queries/country_resolver.py
+++ src/api/resolvers/queries/country_resolver.py
@@ -1,6 +1,12 @@
 from strawberry.types import Info
 
-from src.api.resolvers.context import ContextType, RootValueType, get_session
+from src.api.auth.user_authenticator import authenticate_user
+from src.api.resolvers.context import (
+    ContextType,
+    RootValueType,
+    get_session,
+    get_token,
+)
 from src.api.resolvers.stmts import country_stmt
 from src.api.schema.types import GetCountriesPayload, GetCountryPayload
 
@@ -8,7 +14,9 @@ from src.api.schema.types import GetCountriesPayload, GetCountryPayload
 async def get_countries(
     info: Info[ContextType, RootValueType]
 ) -> GetCountriesPayload:
+    token = get_token(info)
     session = get_session(info)
+    user = await authenticate_user(session=session, token=token)
     countries = await country_stmt.get_countries(session=session)
     return GetCountriesPayload(
         countries=countries, severErrors=[]  # type: ignore
@@ -18,7 +26,9 @@ async def get_countries(
 async def get_country_by_name(
     country_name: str, info: Info[ContextType, RootValueType]
 ) -> GetCountryPayload:
+    token = get_token(info)
     session = get_session(info)
+    user = await authenticate_user(session=session, token=token)
     country = await country_stmt.get_country_by_name(
         session=session, country_name=country_name
     )
